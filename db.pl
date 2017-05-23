af588f53a7ceee343fd162a89d8f20c1fad6b07d
jpporta <Jpedro.porta@gmail.com>
2017-05-23 12:29:47 -0300

    0.0.1

--- db.pl
+++ db.pl
@@ -1,13 +1,21 @@
 %voo(origem,destino,código,partida,(dia_chegada,horario_chegada), número_de_escalas, companhia,[dias]).)
 
-% 1- Verificar se eh possivel ir de uma cidade a outra sem escalas
-%voo_direto(origem, destino, companhia ,dia, Horário).
+% 1- (1 ponto) Verificar se eh possivel ir de uma cidade a outra sem escalas
+voo_direto(Origem, Destino, Companhia ,Dia, Horário):-
+  voo(Origem, Destino, _, _, 0, _, _, _).
 
-% 2- 2)	(1,5 pontos) É possível viajar de uma cidade X a outra Y, ainda que seja necessário fazer conexões? Independente do tempo da conexão (poderia ser de até uma semana). A sua função deve retornar a lista códigos de vôos que serão realizados.
-% roteiro(Origem, Destino, ListaVoos).
+% 2- 	(1,5 pontos) É possível viajar de uma cidade X a outra Y, ainda que seja necessário fazer conexões? Independente do tempo da conexão (poderia ser de até uma semana). A sua função deve retornar a lista códigos de vôos que serão realizados.
+roteiro(Origem, Destino, ListaVoos):-
+  voo(Origem, Destino, Codigo, Partida, Chegada, _, _, _),
+  menor_semana(Partida, Chegada),
+  insere(ListaVoos, Codigo).
+
 
 % 3-	(1,5 pontos) Existe vôo direto entre duas cidades, num determinado dia da semana? A sua consulta deve retornar o dia da semana, o horário de saída, o horário de chegada e a companhia.
-% filtra_voo_dia_semana(Origem,Destino, DiaSemana, HorarioSaida,HorarioChegada,Companhia).
+filtra_voo_dia_semana(Origem,Destino, DiaSemana, HorarioSaida,HorarioChegada,Companhia):-
+  voo(Origem, Destino, HorarioSaida, (_, HorarioChegada), _, Companhia, Dias),
+    pertence(DiaSemana, Dias).
+  
 
 % 4-	(3 pontos) Qual é o vôo direto de menor duração entre duas cidades dadas, num determinado dia da semana? Sua consulta deve retornar dia da semana, horário de saída, horário de chegada e a companhia.
 % ?-menorDuracao(Origem,Destino,dia,HorarioSaida,HorarioChegada,Companhia)
