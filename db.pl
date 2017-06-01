f1b862da2660b1e4284e6eaed07bd7ef54ed40f9
jpporta <Jpedro.porta@gmail.com>
2017-06-01 11:18:56 -0300

    Bosta ai 0.2

--- db.pl
+++ db.pl
@@ -1,4 +1,5 @@
 cls :- write('\e[H\e[2J').%clear screen
+
 %----------------- DEFINES ----------------
 voo(sao-paulo, mexico, código,partida,(dia_chegada,horario_chegada), número_de_escalas, companhia,[dias]).
 voo(sao-paulo, nova-york, código,partida,(dia_chegada,horario_chegada), número_de_escalas, companhia,[dias]).
@@ -26,34 +27,44 @@ voo(frankfurt, roma, código,partida,(dia_chegada,horario_chegada), número_de_e
 
 %%----------------- FUNÇÕES ----------------
 %voo(origem,destino,código,partida,(dia_chegada,horario_chegada), número_de_escalas, companhia,[dias]).
-pertence(X,[X|Calda]).
-pertence(X,[W|Calda]):-
+pertence(X,[X|_]).
+pertence(X,[_|Calda]):-
   pertence(X,Calda).
 
+conc([],L,L).
+conc([X|L1],L2,[X|L]) :-
+	conc(L1,L2,L).
+
 % 1- (1 ponto) Verificar se eh possivel ir de uma cidade a outra sem escalas
-voo_direto(Origem, Destino, Companhia ,Dia, Horário):-
+voo_direto(Origem, Destino, _ ,_, _):-
   voo(Origem, Destino, _, _, 0, _, _, _).
 
 % 2- 	(1,5 pontos) É possível viajar de uma cidade X a outra Y, ainda que seja necessário fazer conexões?
 % Independente do tempo da conexão (poderia ser de até uma semana). A sua função deve retornar a lista códigos de vôos que serão realizados.
-roteiro(Origem, Destino, ListaVoos).
+
+
+  
 
 
 
 % 3-	(1,5 pontos) Existe vôo direto entre duas cidades, num determinado dia da semana?
 % A sua consulta deve retornar o dia da semana, o horário de saída, o horário de chegada e a companhia.
 filtra_voo_dia_semana(Origem,Destino, DiaSemana, HorarioSaida,HorarioChegada,Companhia):-
-  voo(Origem, Destino, _, Partida, Chegada, 0, Companhia, Dias),
+  voo(Origem, Destino, _, HorarioSaida, (_, HorarioChegada), 0, Companhia, Dias),
   pertence(Dias, DiaSemana).
 
 
+%voo(origem,destino,código,partida,(dia_chegada,horario_chegada), número_de_escalas, companhia,[dias]).///////////////////////
 
 % 4-	(3 pontos) Qual é o vôo direto de menor duração entre duas cidades dadas, num determinado dia da semana?
 % Sua consulta deve retornar dia da semana, horário de saída, horário de chegada e a companhia.
- menorDuracao(Origem,Destino,*Dia,*HorarioSaida,*HorarioChegada,*Companhia):-
-   roteiro(Origem, Destino),
-   menorDuration()
+
+ menorDuracao(Origem,Destino,Dia,HorarioSaida,HorarioChegada,Companhia):-
+   filtra_voo_dia_semana(Origem, Destino, Dia, HorarioSaida, HorarioChegada, Companhia),
+   menorDuracao(Origem, Destino, Dia, Hs, Hc, _),
+   HorarioChegada - HorarioSaida =< Hc - Hs.
+   
 
 
 % 5-	(3 pontos) Idem ao anterior, mostrando o dia e horário de partida e a duração total da viagem.
-%  menor_roteiro(Origem, Destino, DiaSaída, HorSaida, Duração).
+menor_roteiro(Origem, Destino, DiaSaída, HorSaida, Duração).
